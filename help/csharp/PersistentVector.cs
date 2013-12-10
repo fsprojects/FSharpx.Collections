476b6c92ddf6acda5c8be776aff9ece065a0d603
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-12-10 14:29:34 +0100

    Add C# docs for PersistentHashMap - references #2

--- help/csharp/PersistentVector.cs
+++ help/csharp/PersistentVector.cs
@@ -14,28 +14,35 @@ namespace CSharp
 
 			// [create-vector]
             // Create an empty PersistentVector and add some elements
-            PersistentVector<string> v =
-		        PersistentVector<string>.Empty()
-		            .Conj("hello")
-		            .Conj("world")
-		            .Conj("!");
+            PersistentVector<string> vector =
+                PersistentVector<string>.Empty()
+                    .Conj("hello")
+                    .Conj("world")
+                    .Conj("!");
 
-            Console.WriteLine(v[0]); // hello
-            Console.WriteLine(v[2]); // !            
+            Console.WriteLine(vector[0]); // hello
+            Console.WriteLine(vector[2]); // !            
 
             // Check no. of elements in the PersistentVector
-            Console.WriteLine(v.Length);  // 3
+            Console.WriteLine(vector.Length);  // 3
+
 		    // [/create-vector]
 
-            // [modify-vector]            
-            PersistentVector<string> v2 = v.Conj("!!!").Update(0,"hi");
+            // [modify-vector]
+            PersistentVector<string> vector2 = vector.Conj("!!!").Update(0,"hi");
 
-            Console.WriteLine(v2[0]); // hi
-            Console.WriteLine(v[0]);  // hello
-            Console.WriteLine(v2[3]); // !!!
+            Console.WriteLine(vector2[0]); // hi
+            Console.WriteLine(vector[0]);  // hello
+            Console.WriteLine(vector2[3]); // !!!
 
-            Console.WriteLine(v.Length);  // 3
-            Console.WriteLine(v2.Length); // 4
+            Console.WriteLine(vector.Length);  // 3
+            Console.WriteLine(vector2.Length); // 4
+
+            // remove the last element from a PersistentVector
+            PersistentVector<string> vector3 = vector2.Initial;
+
+            Console.WriteLine(vector3.Length); // 3
+
             // [/modify-vector]
 		}
 	}
