d43dc3179ad4841bba4ac8b08c8e4fdf87e16d57
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-12-10 12:57:07 +0100

    Change index.md to index.fsx - references #2

---
+++ help/index.fsx
@@ -0,0 +1,27 @@
+(*** hide ***)
+#r "../build/FSharpx.Collections.dll"
+open System
+
+
+(**
+FSharpx.Collections
+===================
+
+**FSharpx.Collections** is a collection of datastructures for use with F# and C#. 
+
+Please contribute to this project. Don't ask for permission, just fork the repository and send pull requests.
+
+Please also join the [F# Open Source Group](http://fsharp.github.com)
+
+<div class="row">
+  <div class="span1"></div>
+  <div class="span6">
+    <div class="well well-small" id="nuget">
+      The F# DataFrame library can be <a href="https://nuget.org/packages/Deedle">installed from NuGet</a>:
+      <pre>PM> Install-Package Deedle</pre>
+    </div>
+  </div>
+  <div class="span1"></div>
+</div>
+
+*)
--- help/index.md
+++
@@ -1,9 +0,0 @@
-# FSharpx.Collections
-
-**FSharpx.Collections** is a collection of datastructures for use with F# and C#. 
-
-See [the home page](http://forki.github.io/FSharpx.Collections/) and [API docs](http://forki.github.io/FSharpx.Collections/apidocs/index.html) for details.
-
-Please contribute to this project. Don't ask for permission, just fork the repository and send pull requests.
-
-Please also join the [F# Open Source Group](http://fsharp.github.com)
