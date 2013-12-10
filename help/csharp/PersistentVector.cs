6bea1ed3d3734ef461428fd42db5c854759adb14
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-12-10 14:10:25 +0100

    Add docs for C#

--- help/csharp/PersistentVector.cs
+++ help/csharp/PersistentVector.cs
@@ -1,4 +1,5 @@
-﻿using FSharpx.Collections;
+﻿using System;
+using FSharpx.Collections;
 using System.Runtime.CompilerServices;
 
 namespace CSharp
@@ -12,9 +13,30 @@ namespace CSharp
 			// ------------------------------------------------------------
 
 			// [create-vector]
-			// Create an empty PersistentVector
-		    var v = PersistentVector<int>.Empty();
+            // Create an empty PersistentVector and add some elements
+            PersistentVector<string> v =
+		        PersistentVector<string>.Empty()
+		            .Conj("hello")
+		            .Conj("world")
+		            .Conj("!");
+
+            Console.WriteLine(v[0]); // hello
+            Console.WriteLine(v[2]); // !            
+
+            // Check no. of elements in the PersistentVector
+            Console.WriteLine(v.Length);  // 3
 		    // [/create-vector]
+
+            // [modify-vector]            
+            PersistentVector<string> v2 = v.Conj("!!!").Update(0,"hi");
+
+            Console.WriteLine(v2[0]); // hi
+            Console.WriteLine(v[0]);  // hello
+            Console.WriteLine(v2[3]); // !!!
+
+            Console.WriteLine(v.Length);  // 3
+            Console.WriteLine(v2.Length); // 4
+            // [/modify-vector]
 		}
 	}
 }
