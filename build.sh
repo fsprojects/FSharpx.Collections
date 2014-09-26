d0c45ac7dd1d7c873f8e507d1828f29745acf3f4
F# Software Foundation <fsgit@fastmail.fm>
2014-09-26 18:03:18 +0100

    fix travis build

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+mono .paket/paket.bootstrapper.exe
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+	exit $exit_code
+fi
+
+mono .paket/paket.exe install -v
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+	exit $exit_code
+fi
+
+mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
