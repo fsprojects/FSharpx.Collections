4fcc9572a6b48ecb8920a27e9efcd353b6d501fd
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-03-22 11:22:19 +0100

    Update to latest FSF

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -3,8 +3,6 @@
 // (the generated documentation is stored in the 'docs/output' directory)
 // --------------------------------------------------------------------------------------
 
-// Binaries that have XML documentation (in a corresponding generated XML file)
-let referenceBinaries = [ "FSharpx.Collections.dll" ]
 // Web site location for the generated documentation
 let website = "/FSharpx.Collections"
 
@@ -22,14 +20,10 @@ let info =
 // For typical project, no changes are needed below
 // --------------------------------------------------------------------------------------
 
-#I "../../packages/FSharp.Formatting/lib/net40"
-#I "../../packages/FSharp.Compiler.Service/lib/net40"
-#r "../../packages/FAKE/tools/FakeLib.dll"
-#r "System.Web.Razor.dll"
-#r "RazorEngine.dll"
-#r "FSharp.Literate.dll"
-#r "FSharp.CodeFormat.dll"
-#r "FSharp.MetadataFormat.dll"
+#I "../../packages/FAKE/tools/"
+#load "../../packages/FSharp.Formatting/FSharp.Formatting.fsx"
+#r "NuGet.Core.dll"
+#r "FakeLib.dll"
 open Fake
 open System.IO
 open Fake.FileHelper
@@ -46,7 +40,6 @@ let root = "file://" + (__SOURCE_DIRECTORY__ @@ "../output")
 
 // Paths with template/source/output locations
 let bin        = __SOURCE_DIRECTORY__ @@ "../../bin"
-let bin35        = __SOURCE_DIRECTORY__ @@ "../../bin/net35"
 let content    = __SOURCE_DIRECTORY__ @@ "../content"
 let output     = __SOURCE_DIRECTORY__ @@ "../output"
 let files      = __SOURCE_DIRECTORY__ @@ "../files"
@@ -55,62 +48,89 @@ let formatting = __SOURCE_DIRECTORY__ @@ "../../packages/FSharp.Formatting/"
 let docTemplate = formatting @@ "templates/docpage.cshtml"
 
 // Where to look for *.csproj templates (in this order)
-let layoutRoots =
-  [ templates; formatting @@ "templates"
-    formatting @@ "templates/reference" ]
+let layoutRootsAll = new System.Collections.Generic.Dictionary<string, string list>()
+layoutRootsAll.Add("en",[ templates; formatting @@ "templates"
+                          formatting @@ "templates/reference" ])
+subDirectories (directoryInfo templates)
+|> Seq.iter (fun d ->
+                let name = d.Name
+                if name.Length = 2 || name.Length = 3 then
+                    layoutRootsAll.Add(
+                            name, [templates @@ name
+                                   formatting @@ "templates"
+                                   formatting @@ "templates/reference" ]))
 
 // Copy static files and CSS + JS from F# Formatting
-let copyFiles () =  
+let copyFiles () =
   CopyRecursive files output true |> Log "Copying file: "
   ensureDirectory (output @@ "content")
   CopyRecursive (formatting @@ "styles") (output @@ "content") true 
     |> Log "Copying styles and scripts: "
 
+let references =
+  if isMono then
+    // Workaround compiler errors in Razor-ViewEngine
+    let d = RazorEngine.Compilation.ReferenceResolver.UseCurrentAssembliesReferenceResolver()
+    let loadedList = d.GetReferences () |> Seq.map (fun r -> r.GetFile()) |> Seq.cache
+    // We replace the list and add required items manually as mcs doesn't like duplicates...
+    let getItem name = loadedList |> Seq.find (fun l -> l.Contains name)
+    [ (getItem "FSharp.Core").Replace("4.3.0.0", "4.3.1.0")
+      Path.GetFullPath "./../../packages/FSharp.Compiler.Service/lib/net40/FSharp.Compiler.Service.dll"
+      Path.GetFullPath "./../../packages/FSharp.Formatting/lib/net40/System.Web.Razor.dll"
+      Path.GetFullPath "./../../packages/FSharp.Formatting/lib/net40/RazorEngine.dll"
+      Path.GetFullPath "./../../packages/FSharp.Formatting/lib/net40/FSharp.Literate.dll"
+      Path.GetFullPath "./../../packages/FSharp.Formatting/lib/net40/FSharp.CodeFormat.dll"
+      Path.GetFullPath "./../../packages/FSharp.Formatting/lib/net40/FSharp.MetadataFormat.dll" ]
+    |> Some
+  else None
+
+let binaries =
+    directoryInfo bin 
+    |> subDirectories
+    |> Array.map (fun d -> d.FullName @@ (sprintf "%s.dll" d.Name))
+    |> List.ofArray
+
+let libDirs =
+    directoryInfo bin 
+    |> subDirectories
+    |> Array.map (fun d -> d.FullName)
+    |> List.ofArray
+
 // Build API reference from XML comments
 let buildReference () =
   CleanDir (output @@ "reference")
-  for lib in referenceBinaries do
-    MetadataFormat.Generate
-      ( bin35 @@ lib, output @@ "reference", layoutRoots, 
-        parameters = ("root", root)::info,
-        sourceRepo = githubLink @@ "tree/master",
-        sourceFolder = __SOURCE_DIRECTORY__ @@ ".." @@ "..",
-        publicOnly = true )
+  MetadataFormat.Generate
+    ( binaries, output @@ "reference", layoutRootsAll.["en"],
+      parameters = ("root", root)::info,
+      sourceRepo = githubLink @@ "tree/master",
+      sourceFolder = __SOURCE_DIRECTORY__ @@ ".." @@ "..",
+      ?assemblyReferences = references,
+      publicOnly = true,libDirs = libDirs )
 
 // Build documentation from `fsx` and `md` files in `docs/content`
 let buildDocumentation () =
   let subdirs = Directory.EnumerateDirectories(content, "*", SearchOption.AllDirectories)
   for dir in Seq.append [content] subdirs do
     let sub = if dir.Length > content.Length then dir.Substring(content.Length + 1) else "."
+    let langSpecificPath(lang, path:string) =
+        path.Split([|'/'; '\\'|], System.StringSplitOptions.RemoveEmptyEntries)
+        |> Array.exists(fun i -> i = lang)
+    let layoutRoots =
+        let key = layoutRootsAll.Keys |> Seq.tryFind (fun i -> langSpecificPath(i, dir))
+        match key with
+        | Some lang -> layoutRootsAll.[lang]
+        | None -> layoutRootsAll.["en"] // "en" is the default language
     Literate.ProcessDirectory
       ( dir, docTemplate, output @@ sub, replacements = ("root", root)::info,
-        layoutRoots = layoutRoots )
-
-// Remove `FSharp.Core` from `bin` directory.
-// Otherwise, version conflict can break code tips.
-let execute pipeline =
-    // Cache `FSharp.Core.*` files
-    let files = 
-        !! (bin @@ "FSharp.Core.*")
-        |> Seq.toArray
-        |> Array.map (fun file ->
-            (file, File.ReadAllBytes file))
-    if (files.Length > 0) then
-        TraceHelper.traceError "Consider setting CopyLocal to False for FSharp.Core in all *.fsproj files"
-    // Remove `FSharp.Core.*` files
-    files |> Seq.iter (fun (file,_) ->
-        TraceHelper.traceImportant <| sprintf  "Removing '%s'" file
-        File.Delete file)
-    // Execute document generation pipeline
-    pipeline()
-    // Restore `FSharp.Core.*` files
-    files |> Seq.iter (fun (file, bytes) ->
-        TraceHelper.traceImportant <| sprintf "Restoring '%s'" file
-        File.WriteAllBytes(file, bytes))
-
+        layoutRoots = layoutRoots,
+        ?assemblyReferences = references,
+        generateAnchors = true )
 
 // Generate
-execute(
-  copyFiles 
-  >> buildDocumentation
-  >> buildReference)
+copyFiles()
+#if HELP
+buildDocumentation()
+#endif
+#if REFERENCE
+buildReference()
+#endif
--- src/FSharpx.Collections/AssemblyInfo.fs
+++ src/FSharpx.Collections/AssemblyInfo.fs
@@ -5,11 +5,11 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("FSharpx.Collections")>]
 [<assembly: AssemblyProductAttribute("FSharpx.Collections")>]
 [<assembly: AssemblyDescriptionAttribute("FSharpx.Collections is a collection of datastructures for use with F# and C#.")>]
-[<assembly: AssemblyVersionAttribute("1.10.0")>]
+[<assembly: AssemblyVersionAttribute("1.10.1")>]
 [<assembly: InternalsVisibleToAttribute("FSharpx.Collections.Tests")>]
 [<assembly: InternalsVisibleToAttribute("FSharpx.Collections.Experimental.Tests")>]
-[<assembly: AssemblyFileVersionAttribute("1.10.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.10.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.10.0"
+    let [<Literal>] Version = "1.10.1"
