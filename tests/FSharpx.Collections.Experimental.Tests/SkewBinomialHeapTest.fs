d2f9208f6493735fb991296a8d4006e846eb0c72
Enrique Alfonso Casanovas Pedre <eacasanovaspedre@yandex.com>
2015-06-22 05:15:02 -0400

    Improved one case generator and some minor changes to some tests

--- tests/FSharpx.Collections.Experimental.Tests/SkewBinomialHeapTest.fs
+++ tests/FSharpx.Collections.Experimental.Tests/SkewBinomialHeapTest.fs
@@ -27,14 +27,18 @@ type ComparableGenerator<'T when 'T :> IComparable> =
 type HeapData<'T, 'L when 'T: comparison> = { Heap: 'T SkewBinomialHeap; Items: 'L list; Desc: bool }
 
 type SkewBinomialHeapGenerators() =
+    static let genDesc d = 
+        match d with
+        | Some v -> Gen.constant v
+        | None -> Gen.elements [true; false]
     // Empty heaps only, some are empty after removing all of its elements
-    static member private EmptyOnly<'T when 'T: comparison> (): Gen<HeapData<'T, 'T>> = gen{
-        let! desc = Gen.elements [true; false]
+    static member private EmptyOnly<'T when 'T: comparison> (d): Gen<HeapData<'T, 'T>> = gen{
+        let! desc = genDesc d
         let! s = Gen.listOf (Arb.generate<'T>)
         return { Heap = Seq.init (s.Length) id |> Seq.fold (fun heap _ -> heap |> SkewBinomialHeap.tail) (ofList desc s); Items = []; Desc = desc }}
     // Non-emtpy heaps only, after a sucession of insertions and deletions
-    static member private NonemptyOnly<'T when 'T: comparison> (): Gen<HeapData<'T, 'T>> = gen{
-        let! desc = Gen.elements [true; false]
+    static member private NonemptyOnly<'T when 'T: comparison> (d): Gen<HeapData<'T, 'T>> = gen{
+        let! desc = genDesc d
         let! t = Gen.elements [true; false]
         let! s = Gen.nonEmptyListOf (Arb.generate<'T>)
         let! ndel = if t then Gen.constant Int32.MaxValue else Gen.choose (2, max 2 (s |> List.length))
@@ -52,20 +56,15 @@ type SkewBinomialHeapGenerators() =
         return {Heap = heap; Items = list; Desc = desc}}
     
     static member private Mixed<'T when 'T: comparison> (): Gen<HeapData<'T, 'T>> =
-        Gen.frequency [200, SkewBinomialHeapGenerators.EmptyOnly<'T> (); 800, SkewBinomialHeapGenerators.NonemptyOnly<'T> ()]
+        Gen.frequency [200, SkewBinomialHeapGenerators.EmptyOnly<'T> (None); 800, SkewBinomialHeapGenerators.NonemptyOnly<'T> (None)]
 
+    //Distribute the cases, so the tests that receive two heaps and need both to have the same isDescending don't exhaust the arguments, ex: the merge tests
     static member private TwoMixed<'T when 'T: comparison> (): Gen<HeapData<'T, 'T> * HeapData<'T, 'T>> =
         Gen.frequency [
-            200, SkewBinomialHeapGenerators.EmptyOnly<'T> () |> Gen.two; 
-            400, SkewBinomialHeapGenerators.NonemptyOnly<'T> () |> Gen.two;
-            200, gen{ 
-                let! e = SkewBinomialHeapGenerators.EmptyOnly<'T>()
-                let! n = SkewBinomialHeapGenerators.EmptyOnly<'T>()
-                return (e, n)}
-            200, gen{ 
-                let! e = SkewBinomialHeapGenerators.EmptyOnly<'T>()
-                let! n = SkewBinomialHeapGenerators.EmptyOnly<'T>()
-                return (n, e)}]
+            500, Gen.oneof [SkewBinomialHeapGenerators.EmptyOnly<'T> (None); SkewBinomialHeapGenerators.NonemptyOnly<'T> (None)] |> Gen.two
+            250, Gen.oneof [SkewBinomialHeapGenerators.EmptyOnly<'T> (Some true); SkewBinomialHeapGenerators.NonemptyOnly<'T> (Some true)] |> Gen.two
+            250, Gen.oneof [SkewBinomialHeapGenerators.EmptyOnly<'T> (Some false); SkewBinomialHeapGenerators.NonemptyOnly<'T> (Some false)] |> Gen.two
+            ]
 
     static member ComparableAndComparable() = SkewBinomialHeapGenerators.Mixed<IComparable> () |> Arb.fromGen
 
@@ -95,6 +94,7 @@ let fsCheck a = fsCheck null a
 
 //************* TESTS *****************
 //Only tested the functions on the SkewBinomialHeap module for now, later I could test the remaining functions and methods
+//TODO: Test ofSeq
 
 [<Test>]
 let ``toSeq returns all the elements`` () =
@@ -139,7 +139,7 @@ let ``length is the same as count`` () =
 [<Test>]
 let ``head returns the first element when the heap is not empty`` () =
     fsCheck <| fun { Heap = heap; Items = orig } -> 
-        heap |> SkewBinomialHeap.isEmpty |> not ==> lazy(heap |> SkewBinomialHeap.head |> should equal <| actualHead heap orig)
+        heap |> SkewBinomialHeap.isEmpty |> not ==> lazy(heap |> SkewBinomialHeap.head = actualHead heap orig)
 
 [<Test>]
 let ``head throws when the heap is empty`` () =
@@ -150,20 +150,19 @@ let ``head throws when the heap is empty`` () =
 let ``tryHead returns Some head when the heap is not empty`` () =
     fsCheck <| fun { Heap = heap; Items = orig } -> 
         heap |> SkewBinomialHeap.isEmpty |> not ==> 
-            lazy(
-                match SkewBinomialHeap.tryHead heap with 
-                | Some head -> head |> should equal <| actualHead heap orig
-                | None -> fail "tryHead to a non-empty heap returned None")
+        match SkewBinomialHeap.tryHead heap with 
+        | Some head -> head = actualHead heap orig
+        | None -> false
 
 [<Test>]
 let ``tryHead returns None when the heap is empty`` () =
     fsCheck <| fun { Heap = heap } ->
-        heap |> SkewBinomialHeap.isEmpty ==> lazy (heap |> SkewBinomialHeap.tryHead |> Option.isNone)
+        heap |> SkewBinomialHeap.isEmpty ==> (heap |> SkewBinomialHeap.tryHead |> Option.isNone)
 
 [<Test>]
 let ``tail returns a heap with the first element removed when the heap is not empty`` () =
     fsCheck <| fun { Heap = heap; Items = orig } ->
-        heap |> SkewBinomialHeap.isEmpty |> not ==> lazy(heap |> SkewBinomialHeap.tail |> SkewBinomialHeap.toList |> should equal <| actualTail heap orig)
+        heap |> SkewBinomialHeap.isEmpty |> not ==> lazy(heap |> SkewBinomialHeap.tail |> SkewBinomialHeap.toList = actualTail heap orig)
 
 [<Test>]
 let ``tail throws when the heap is empty`` () =
@@ -176,13 +175,13 @@ let ``tryTail returns Some tail when the heap is not empty`` () =
         heap |> SkewBinomialHeap.isEmpty |> not ==> 
             lazy(
                 match SkewBinomialHeap.tryTail heap with 
-                | Some tail -> tail |> SkewBinomialHeap.toList |> should equal <| actualTail heap orig
-                | None -> fail "tryTail to a non-empty heap returned None")
+                | Some tail -> tail |> SkewBinomialHeap.toList = actualTail heap orig
+                | None -> false)
 
 [<Test>]
 let ``tryTail returns None when the heap is empty`` () =
     fsCheck <| fun { Heap = heap } ->
-        heap |> SkewBinomialHeap.isEmpty ==> lazy(heap |> SkewBinomialHeap.tryTail |> Option.isNone)
+        heap |> SkewBinomialHeap.isEmpty ==> (heap |> SkewBinomialHeap.tryTail |> Option.isNone)
 
 [<Test>]
 let ``uncons returns (head, tail) when the heap is not empty`` () =
@@ -190,8 +189,8 @@ let ``uncons returns (head, tail) when the heap is not empty`` () =
         heap |> SkewBinomialHeap.isEmpty |> not ==> 
             lazy(
                 let (h, t) = SkewBinomialHeap.uncons heap
-                h |> should equal <| actualHead heap orig
-                t |> SkewBinomialHeap.toList |> should equal <| actualTail heap orig)
+                h = actualHead heap orig &&
+                SkewBinomialHeap.toList t = actualTail heap orig)
 
 [<Test>]
 let ``uncons throws when the heap is empty`` () =
@@ -205,9 +204,9 @@ let ``tryUncons returns Some (head, tail) when the heap is not empty`` () =
             lazy(
                 match SkewBinomialHeap.tryUncons heap with 
                 | Some (h, t) ->
-                    h |> should equal (actualHead heap orig)
-                    t |> SkewBinomialHeap.toList |> should equal (actualTail heap orig)
-                | None -> fail "tryUncons to a non-empty heap returned None")
+                    h = actualHead heap orig &&
+                    SkewBinomialHeap.toList t = actualTail heap orig
+                | None -> false)
 
 [<Test>]
 let ``tryUncons returns None when the heap is empty`` () =
@@ -224,7 +223,7 @@ let ``insert always insert`` () =
 
 [<Test>]
 let ``merge should merge if both heaps have the same ordering`` () =
-    fsCheck <| fun { Heap = heap1; Items = orig1 } { Heap = heap2; Items = orig2 } ->
+    fsCheck <| fun ({ Heap = heap1; Items = orig1 }, { Heap = heap2; Items = orig2 }) ->
         heap1.IsDescending = heap2.IsDescending ==>
         lazy(
             SkewBinomialHeap.merge heap1 heap2 
@@ -233,22 +232,22 @@ let ``merge should merge if both heaps have the same ordering`` () =
 
 [<Test>]
 let ``merge throws when both heaps have diferent ordering`` () =
-    fsCheck <| fun { Heap = heap1 } { Heap = heap2 } ->
+    fsCheck <| fun ({ Heap = heap1 }, { Heap = heap2 }) ->
         heap1.IsDescending <> heap2.IsDescending ==>
         lazy(should throw typeof<IncompatibleMerge> <| fun () -> SkewBinomialHeap.merge heap1 heap2 |> ignore)
 
 [<Test>]
 let ``tryMerge returns Some merged heap when both heaps have the same ordering`` () =
-    fsCheck <| fun { Heap = heap1; Items = orig1 } { Heap = heap2; Items = orig2 } ->
+    fsCheck <| fun ({ Heap = heap1; Items = orig1 }, { Heap = heap2; Items = orig2 }) ->
         heap1.IsDescending = heap2.IsDescending ==>
         lazy(
             match SkewBinomialHeap.tryMerge heap1 heap2 |> Option.map SkewBinomialHeap.toList with
-            | Some list -> list |> should equal (orig1 |> List.append orig2 |> sortList heap1)
-            | None -> fail "tryMerge returned None when called with two compatible heaps")
+            | Some list -> list = (List.append orig1 orig2 |> sortList heap1)
+            | None -> false)
 
 [<Test>]
 let ``tryMerge returns None when both heaps have diferent ordering`` () =
-    fsCheck <| fun { Heap = heap1; Items = orig1 } { Heap = heap2; Items = orig2 } ->
+    fsCheck <| fun ({ Heap = heap1; Items = orig1 }, { Heap = heap2; Items = orig2 }) ->
         heap1.IsDescending <> heap2.IsDescending ==> lazy(SkewBinomialHeap.tryMerge heap1 heap2 |> Option.isNone)
 
 [<Test>]
@@ -269,10 +268,40 @@ let ``Nil pattern always match if the heap is empty`` () =
 
 [<Test>]
 let ``Cons pattern return the same as uncons`` () =
-    fsCheck <| fun { Heap = heap; Items = orig } ->
+    fsCheck <| fun { Heap = heap } ->
         heap |> SkewBinomialHeap.isEmpty |> not ==>
         match heap with
         | SkewBinomialHeap.Cons (h, t) ->
             let (h', t') = heap |> SkewBinomialHeap.uncons
-            h = h' && SkewBinomialHeap.toList t = SkewBinomialHeap.toList t'
+            h = h' && t = t'
         | _ -> false
+
+[<Test>]
+let ``GetHashCode is the same for equal heaps`` () =
+    fsCheck <| fun { Heap = heap } item ->
+        let heap1 = heap |> SkewBinomialHeap.insert item |> SkewBinomialHeap.tail
+        let heap2 = heap |> SkewBinomialHeap.insert item |> SkewBinomialHeap.tail
+        Unchecked.hash heap1 = Unchecked.hash heap2
+
+//Maybe the distribution of the hash should be checked
+//to avoid bad hashes, I don't know if that should be done as part of unit testing
+
+[<Test>]
+let ``Equality reflexivity`` () =
+    fsCheck <| fun { Heap = heap } ->
+        heap = heap
+
+[<Test>]
+let ``Equality symmetry`` () =
+    fsCheck <| fun {Heap = heap } item ->
+        let heap1 = heap |> SkewBinomialHeap.insert item |> SkewBinomialHeap.tail
+        let heap2 = heap |> SkewBinomialHeap.insert item |> SkewBinomialHeap.tail
+        heap1 = heap2 ==> (heap2 = heap1)
+
+[<Test>]
+let ``Equality transitivity`` () = //maybe this is too much, I guess It would be hard to write an Equals that violates this property and not the others
+    fsCheck <| fun {Heap = heap } item ->
+        let heap1 = heap |> SkewBinomialHeap.insert item |> SkewBinomialHeap.tail
+        let heap2 = heap |> SkewBinomialHeap.insert item |> SkewBinomialHeap.tail
+        let heap3 = heap |> SkewBinomialHeap.insert item |> SkewBinomialHeap.tail
+        (heap1 = heap2 && heap2 = heap3) ==> (heap1 = heap3)
