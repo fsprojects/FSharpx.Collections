baa9503c597566225523e2faa63fb57c3da5c5fb
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-04-25 07:54:35 +0200

    Bump version to 1.15.3

--- src/FSharpx.Collections/AssemblyInfo.fs
+++ src/FSharpx.Collections/AssemblyInfo.fs
@@ -7,10 +7,10 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyDescriptionAttribute("FSharpx.Collections is a collection of datastructures for use with F# and C#.")>]
 [<assembly: InternalsVisibleToAttribute("FSharpx.Collections.Tests")>]
 [<assembly: InternalsVisibleToAttribute("FSharpx.Collections.Experimental.Tests")>]
-[<assembly: AssemblyVersionAttribute("1.15.2")>]
-[<assembly: AssemblyFileVersionAttribute("1.15.2")>]
+[<assembly: AssemblyVersionAttribute("1.15.3")>]
+[<assembly: AssemblyFileVersionAttribute("1.15.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.15.2"
-    let [<Literal>] InformationalVersion = "1.15.2"
+    let [<Literal>] Version = "1.15.3"
+    let [<Literal>] InformationalVersion = "1.15.3"
