bafc3a6acb204931b2fdcec8736b683c18eb2642
Enrico Sada <enrico@sada.io>
2017-09-28 14:16:54 -0700

    dotnet new nunit -lang F#

---
+++ tests/FSharpx.Collections.Tests.NetStandard/UnitTest1.fs
@@ -0,0 +1,14 @@
+namespace FSharpx.Collections.Tests.NetStandard.Tests
+
+open NUnit.Framework
+
+[<TestClass>]
+type TestClass () =
+
+    [<SetUp>]
+    member this.Setup () =
+        ()
+
+    [<Test>]
+    member this.Test1 () =
+        Assert.Pass()
