49a6d5a521817f480c2c805afc07fc2b26c72a91
Jack Fox <jackfoxy@gmail.com>
2018-07-11 08:57:33 -0700

    send expecto test return code to target return

--- tests/FSharpx.Collections.Tests/RunTests.fs
+++ tests/FSharpx.Collections.Tests/RunTests.fs
@@ -8,53 +8,54 @@ module RunTests =
     [<EntryPoint>]
     let main args =
 
-        Tests.runTestsWithArgs defaultConfig args ArrayTests.testArray |> ignore
+        [
+            Tests.runTestsWithArgs defaultConfig args ArrayTests.testArray
 
-        Tests.runTestsWithArgs defaultConfig args ByteStringTests.testByteString |> ignore
+            Tests.runTestsWithArgs defaultConfig args ByteStringTests.testByteString
 
-        Tests.runTestsWithArgs defaultConfig args CircularBufferTests.testCircularBuffer |> ignore
+            Tests.runTestsWithArgs defaultConfig args CircularBufferTests.testCircularBuffer
 
-        Tests.runTestsWithArgs defaultConfig args ResizeArrayTests.testResizeArray |> ignore
+            Tests.runTestsWithArgs defaultConfig args ResizeArrayTests.testResizeArray
 
-        Tests.runTestsWithArgs defaultConfig args DequeTests.testDeque |> ignore
-        Tests.runTestsWithArgs defaultConfig args DequeTests.propertyTestDeque |> ignore
+            Tests.runTestsWithArgs defaultConfig args DequeTests.testDeque
+            Tests.runTestsWithArgs defaultConfig args DequeTests.propertyTestDeque
 
-        Tests.runTestsWithArgs defaultConfig args DictionaryExtensionsTests.testDictionaryExtensions |> ignore
+            Tests.runTestsWithArgs defaultConfig args DictionaryExtensionsTests.testDictionaryExtensions
 
-        Tests.runTestsWithArgs defaultConfig args DListTests.testDList |> ignore
-        Tests.runTestsWithArgs defaultConfig args DListTests.propertyTestDList |> ignore
+            Tests.runTestsWithArgs defaultConfig args DListTests.testDList
+            Tests.runTestsWithArgs defaultConfig args DListTests.propertyTestDList
 
-        Tests.runTestsWithArgs defaultConfig args HeapTests.testHeap |> ignore
-        Tests.runTestsWithArgs defaultConfig args HeapTests.propertyTestHeap |> ignore
+            Tests.runTestsWithArgs defaultConfig args HeapTests.testHeap
+            Tests.runTestsWithArgs defaultConfig args HeapTests.propertyTestHeap
 
-        Tests.runTestsWithArgs defaultConfig args LazyList.testLazyList |> ignore
+            Tests.runTestsWithArgs defaultConfig args LazyList.testLazyList
 
-        Tests.runTestsWithArgs defaultConfig args ListExtensionsTests.testListExtensions |> ignore
-        Tests.runTestsWithArgs defaultConfig args ListExtensionsTests.propertyTestListExtensions |> ignore
+            Tests.runTestsWithArgs defaultConfig args ListExtensionsTests.testListExtensions
+            Tests.runTestsWithArgs defaultConfig args ListExtensionsTests.propertyTestListExtensions
 
-        Tests.runTestsWithArgs defaultConfig args MapExtensionsTests.testMapExtensions |> ignore
+            Tests.runTestsWithArgs defaultConfig args MapExtensionsTests.testMapExtensions
 
-        Tests.runTestsWithArgs defaultConfig args MapTests.testMap |> ignore
+            Tests.runTestsWithArgs defaultConfig args MapTests.testMap
 
-        Tests.runTestsWithArgs defaultConfig args NameValueCollectionTests.testNameValueCollection |> ignore
+            Tests.runTestsWithArgs defaultConfig args NameValueCollectionTests.testNameValueCollection
 
-        Tests.runTestsWithArgs defaultConfig args NonEmptyListTests.testNonEmptyList |> ignore
+            Tests.runTestsWithArgs defaultConfig args NonEmptyListTests.testNonEmptyList
 
-        Tests.runTestsWithArgs defaultConfig args PriorityQueueTests.testPriorityQueue |> ignore
+            Tests.runTestsWithArgs defaultConfig args PriorityQueueTests.testPriorityQueue
 
-        Tests.runTestsWithArgs defaultConfig args QueueTests.testQueue |> ignore
-        Tests.runTestsWithArgs defaultConfig args QueueTests.propertyTestQueue |> ignore
+            Tests.runTestsWithArgs defaultConfig args QueueTests.testQueue
+            Tests.runTestsWithArgs defaultConfig args QueueTests.propertyTestQueue
 
-        Tests.runTestsWithArgs defaultConfig args RandomAccessListTest.testRandomAccessList |> ignore
-        Tests.runTestsWithArgs defaultConfig args RandomAccessListTest.propertyTestRandomAccessList |> ignore
+            Tests.runTestsWithArgs defaultConfig args RandomAccessListTest.testRandomAccessList
+            Tests.runTestsWithArgs defaultConfig args RandomAccessListTest.propertyTestRandomAccessList
 
-        Tests.runTestsWithArgs defaultConfig args SeqTests.testSeq |> ignore
+            Tests.runTestsWithArgs defaultConfig args SeqTests.testSeq
 
-        Tests.runTestsWithArgs defaultConfig args PersistentVectorTests.testPersistentVector |> ignore
+            Tests.runTestsWithArgs defaultConfig args PersistentVectorTests.testPersistentVector
 
-        Tests.runTestsWithArgs defaultConfig args TransientHashMapTests.testTransientHashMap |> ignore
+            Tests.runTestsWithArgs defaultConfig args TransientHashMapTests.testTransientHashMap
 
-        Tests.runTestsWithArgs defaultConfig args PersistentHashMapTests.testPersistentHashMap |> ignore
-
-        0
+            Tests.runTestsWithArgs defaultConfig args PersistentHashMapTests.testPersistentHashMap
+        ]
+        |> List.sum
 
