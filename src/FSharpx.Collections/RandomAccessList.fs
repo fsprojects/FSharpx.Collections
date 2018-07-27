c4263038524f431f7e78486962a8b5cfc33ec433
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2018-07-27 23:24:34 +0300

    The RandomAccessList is serializable. Closes #127. Also, add serialization property tests.

--- src/FSharpx.Collections/RandomAccessList.fs
+++ src/FSharpx.Collections/RandomAccessList.fs
@@ -1,6 +1,8 @@
 ﻿namespace FSharpx.Collections
 
-module internal Literals2 = 
+open System
+
+module internal Literals2 =
 
     [<Literal>]
     let internal blockSizeShift = 5 // TODO: what can we do in 64Bit case?
@@ -14,6 +16,7 @@ module internal Literals2 =
 open System.Threading
 #if FX_NO_THREAD
 #else
+[<Serializable>]
 type NodeR(threadId,array:obj[]) =
     let mutable threadId = threadId
     new() = NodeR(None,Array.create Literals2.blockSize null)
@@ -22,6 +25,7 @@ type NodeR(threadId,array:obj[]) =
     member this.ThreadId = threadId
     member this.SetThread t = threadId <- t
 
+[<Serializable>]
 type internal TransientVect<'T> (count,shift:int,root:NodeR,tail:obj[]) =
     let mutable count = count
     let mutable root = root
@@ -29,7 +33,7 @@ type internal TransientVect<'T> (count,shift:int,root:NodeR,tail:obj[]) =
     let mutable shift = shift
 
     new() = TransientVect<'T>(0,Literals2.blockSizeShift,NodeR.InCurrentThread(),Array.create Literals2.blockSize null)
-    
+
     member internal this.EnsureEditable(node:NodeR) =
         if node.ThreadId = root.ThreadId then node else
         NodeR(root.ThreadId,Array.copy node.Array)
@@ -88,7 +92,7 @@ type internal TransientVect<'T> (count,shift:int,root:NodeR,tail:obj[]) =
             newTail.[0] <- x :> obj
 
             //overflow root?
-            let newRoot = 
+            let newRoot =
                 if (count >>> Literals2.blockSizeShift) > (1 <<< shift) then
                     let newRoot = NodeR(root.ThreadId,Array.create Literals2.blockSize null)
                     newRoot.Array.[0] <- root :> obj
@@ -120,10 +124,10 @@ type internal TransientVect<'T> (count,shift:int,root:NodeR,tail:obj[]) =
     member internal this.TailOff() =
         if count < Literals2.blockSize then 0 else
         ((count - 1) >>> Literals2.blockSizeShift) <<< Literals2.blockSizeShift
-        
-and RandomAccessList<'T> (count,shift:int,root:NodeR,tail:obj[])  =
+
+and [<Serializable>] RandomAccessList<'T> (count,shift:int,root:NodeR,tail:obj[])  =
     let mutable hashCode = None
-    let tailOff = 
+    let tailOff =
         if count < Literals2.blockSize then 0 else
         ((count - 1) >>> Literals2.blockSizeShift) <<< Literals2.blockSizeShift
 
@@ -150,7 +154,7 @@ and RandomAccessList<'T> (count,shift:int,root:NodeR,tail:obj[])  =
 
     override this.Equals(other) =
         match other with
-        | :? RandomAccessList<'T> as y -> 
+        | :? RandomAccessList<'T> as y ->
             if this.Length <> y.Length then false else
             if this.GetHashCode() <> y.GetHashCode() then false else
             Seq.forall2 (Unchecked.equals) this y
@@ -199,8 +203,8 @@ and RandomAccessList<'T> (count,shift:int,root:NodeR,tail:obj[])  =
 
     member internal this.doAssoc(level,node:NodeR,i,x) =
         let ret = NodeR(root.ThreadId,Array.copy node.Array)
-        if level = 0 then 
-            ret.Array.[i &&& Literals2.blockIndexMask] <- x :> obj 
+        if level = 0 then
+            ret.Array.[i &&& Literals2.blockIndexMask] <- x :> obj
         else
             let subidx = (i >>> level) &&& Literals2.blockIndexMask
             ret.Array.[subidx] <- this.doAssoc(level - Literals2.blockSizeShift, node.Array.[subidx] :?> NodeR, i, x) :> obj
@@ -233,13 +237,13 @@ and RandomAccessList<'T> (count,shift:int,root:NodeR,tail:obj[])  =
                         array <- this.ArrayFor i
 
                     yield (array).[i &&& Literals2.blockIndexMask] :?> 'T
-                    i <- i - 1 
+                    i <- i - 1
                 }
-        
-    member this.Cons (x : 'T) = 
+
+    member this.Cons (x : 'T) =
         if count - tailOff < Literals2.blockSize then
             let newTail = Array.append tail [|x:>obj|]
-            RandomAccessList<'T>(count + 1,shift,root,newTail) 
+            RandomAccessList<'T>(count + 1,shift,root,newTail)
         else
             //full tail, push into tree
             let tailNode = NodeR(root.ThreadId,tail)
@@ -257,8 +261,8 @@ and RandomAccessList<'T> (count,shift:int,root:NodeR,tail:obj[])  =
 
     member this.IsEmpty = (count = 0)
 
-    member this.Item 
-        with get i = 
+    member this.Item
+        with get i =
             let k = (count - 1) - i
             let node = this.ArrayFor k
             node.[k &&& Literals2.blockIndexMask] :?> 'T
@@ -283,10 +287,10 @@ and RandomAccessList<'T> (count,shift:int,root:NodeR,tail:obj[])  =
         if count = 0 then failwith "Can't tail empty randomAccessList" else
         if count = 1 then RandomAccessList<'T>.Empty() else
 
-        if count - tailOff > 1 then 
+        if count - tailOff > 1 then
             let mutable newroot = NodeR(Some Thread.CurrentThread.ManagedThreadId, root.Array.Clone() :?> obj[])
             let mutable ret = TransientVect(count - 1, shift, newroot, tail.[0..(tail.Length-1)])
-            ret.persistent() 
+            ret.persistent()
         else
             let newtail = this.ArrayFor(count - 2)
 
@@ -307,7 +311,7 @@ and RandomAccessList<'T> (count,shift:int,root:NodeR,tail:obj[])  =
 
     member this.TryUncons = if count > 0 then Some(this.[0], this.Tail)  else None
 
-    member this.Update(i, x : 'T) = 
+    member this.Update(i, x : 'T) =
         let k = (count - 1) - i
         if k >= 0 && k < count then
             if k >= tailOff then
@@ -316,7 +320,7 @@ and RandomAccessList<'T> (count,shift:int,root:NodeR,tail:obj[])  =
                 RandomAccessList(count, shift, root, newTail)
             else
                 RandomAccessList(count, shift, this.doAssoc(shift, root, k, x),tail)
-        elif k = count then this.Cons x 
+        elif k = count then this.Cons x
         else raise (new System.IndexOutOfRangeException())
 
     member this.TryUpdate(i, x : 'T) =
@@ -333,7 +337,7 @@ and RandomAccessList<'T> (count,shift:int,root:NodeR,tail:obj[])  =
             :> System.Collections.IEnumerator
 
 [<RequireQualifiedAccess>]
-module RandomAccessList = 
+module RandomAccessList =
     //pattern discriminators  (active pattern)
     let (|Cons|Nil|) (v : RandomAccessList<'T>) = match v.TryUncons with Some(a,b) -> Cons(a,b) | None -> Nil
 
@@ -349,25 +353,25 @@ module RandomAccessList =
 
     let empty<'T> = RandomAccessList.Empty() :> RandomAccessList<'T>
 
-    let inline fold (f : ('State -> 'T -> 'State)) (state : 'State) (v : RandomAccessList<'T>) = 
+    let inline fold (f : ('State -> 'T -> 'State)) (state : 'State) (v : RandomAccessList<'T>) =
         let rec loop state' (v' : RandomAccessList<'T>) count =
             match count with
             | _ when count = v'.Length -> state'
-            | _ -> loop (f state' v'.[count]) v' (count + 1)  
+            | _ -> loop (f state' v'.[count]) v' (count + 1)
         loop state v 0
 
-    let inline foldBack (f : ('T -> 'State -> 'State)) (v : RandomAccessList<'T>) (state : 'State) =  
+    let inline foldBack (f : ('T -> 'State -> 'State)) (v : RandomAccessList<'T>) (state : 'State) =
         let rec loop state' (v' : RandomAccessList<'T>) count =
             match count with
             | -1 -> state'
-            | _ -> loop (f v'.[count] state') v' (count - 1)  
+            | _ -> loop (f v'.[count] state') v' (count - 1)
         loop state v (v.Length - 1)
 
     let init count (f: int -> 'T) : 'T RandomAccessList =
         let mutable ret = TransientVect()
         for i in 0..(count-1) do
             ret <- ret.conj(f i)
-        ret.persistent().Rev() 
+        ret.persistent().Rev()
 
     let inline isEmpty (randomAccessList :'T RandomAccessList) = randomAccessList.IsEmpty
 
@@ -377,14 +381,14 @@ module RandomAccessList =
 
     let inline length (randomAccessList :'T RandomAccessList) : int = randomAccessList.Length
 
-    let map (f : 'T -> 'T1) (randomAccessList :'T RandomAccessList) : 'T1 RandomAccessList = 
+    let map (f : 'T -> 'T1) (randomAccessList :'T RandomAccessList) : 'T1 RandomAccessList =
         let mutable ret = TransientVect()
         for item in randomAccessList do
             ret <- ret.conj(f item)
-        ret.persistent().Rev() 
+        ret.persistent().Rev()
 
     let inline nth i (randomAccessList :'T RandomAccessList) = randomAccessList.[i]
-    
+
     let inline tryNth i (randomAccessList :'T RandomAccessList) =
         if i >= 0 && i < randomAccessList.Length then Some(randomAccessList.[i])
         else None
@@ -396,7 +400,7 @@ module RandomAccessList =
         | Some v' -> tryNth j v'
         | None -> None
 
-    let ofSeq (items : 'T seq) = RandomAccessList.ofSeq items 
+    let ofSeq (items : 'T seq) = RandomAccessList.ofSeq items
 
     let inline rev (randomAccessList :'T RandomAccessList) = randomAccessList.Rev()
 
