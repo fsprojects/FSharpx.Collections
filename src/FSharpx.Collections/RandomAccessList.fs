928f330b14668ecbd25a055b7a82bc2d164238d9
njlr <njlr@users.noreply.github.com>
2020-11-11 20:32:49 +0000

    * Fixes build error outside Fable

--- src/FSharpx.Collections/RandomAccessList.fs
+++ src/FSharpx.Collections/RandomAccessList.fs
@@ -22,14 +22,15 @@ module Thread =
         let ManagedThreadId = 1
 #endif
 
+module internal Utils =
 #if FABLE_COMPILER
-module Array =
-    let clone (x : _ array) : obj =
-        Array.init x.Length (fun i -> x.[i]) :> obj
+    module  Array =
+        let clone (x : _ array) : obj =
+            Array.init x.Length (fun i -> x.[i]) :> obj
 #else
-module Array =
-    let clone (x : _ array) : obj =
-        x.Clone ()
+    module Array =
+        let clone (x : _ array) : obj =
+            x.Clone ()
 #endif
 
 [<Serializable>]
@@ -302,7 +303,7 @@ and [<Serializable>] RandomAccessList<'T> (count,shift:int,root:NodeR,tail:obj[]
         if count = 1 then RandomAccessList<'T>.Empty() else
 
         if count - tailOff > 1 then
-            let mutable newroot = NodeR(Some Thread.CurrentThread.ManagedThreadId, (Array.clone root.Array) :?> obj[])
+            let mutable newroot = NodeR(Some Thread.CurrentThread.ManagedThreadId, (Utils.Array.clone root.Array) :?> obj[])
             let mutable ret = TransientVect(count - 1, shift, newroot, tail.[0..(tail.Length-1)])
             ret.persistent()
         else
